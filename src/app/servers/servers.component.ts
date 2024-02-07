de410e3106d8a017328aae6a0208a19e2da0454b
Rico Kunz <rico.kunz@usu.com>
2024-02-07 13:09:49 +0100

    inline template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template:
+    `
+      <app-server></app-server>
+      <app-server></app-server>
+    `,
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
